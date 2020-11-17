3c858ca1453599379249f6b264875f6d01c79589
ANKIT VERMA <51227238+ankit-verma-au8@users.noreply.github.com>
2020-11-17 17:29:27 +0530

    addedctaxi and non taxi

--- parkinglot.py
+++ parkinglot.py
@@ -2,9 +2,10 @@ import sys
 
 
 class Car:
-    def __init__(self, reg_no, colour):
+    def __init__(self, reg_no, colour, Taxi):
         self.reg_no = reg_no
         self.colour = colour
+        self.taxi = Taxi
 
     def getcolour(self):
         print("My colour is", self.colour)
@@ -14,28 +15,55 @@ class Car:
 
 
 class Parkinglot:
-    def __init__(self, noofslot):
-        self.noofslot = noofslot
-        self.slots = [0 for i in range(self.noofslot)]
-        print("Created Parking lot of Capacity", self.noofslot)
+    def __init__(self, slot_taxi, slot_nt):
+        self.noofslot_taxt = slot_taxi
+        self.noofslot_nt = slot_nt
+        self.slots = []
+        for i in range(self.noofslot_taxt):
+            self.slots.append(0)
+        for i in range(self.noofslot_nt):
+            self.slots.append(-1)
+        print("Created Parking lot of Capacity", len(self.slots))
+        print("Capacity for taxi car is", self.noofslot_taxt)
+        print("Capacity for non taxi car is", self.noofslot_nt)
 
     def park(self, vehicle):
         if len(vehicle.reg_no) != 13:
             print("Not a valid reg.no, Please enter a valid reg. no.")
         else:
-            for i in range(len(self.slots)):
-                if self.slots[i] == 0:
-                    self.slots[i] = vehicle
-                    print("Allocated slot number", i+1)
-                    break
+            if vehicle.taxi == "taxi":
+                for i in range(len(self.slots)):
+                    if self.slots[i] == 0:
+                        self.slots[i] = vehicle
+                        print("Allocated slot number", i+1)
+                        break
+                else:
+                    print("Sorry! No slots avialable.")
+                    print("If need more slots, use command: 'add_slot N'")
+            elif vehicle.taxi == "nontaxi":
+                for i in range(len(self.slots)):
+                    if self.slots[i] == -1:
+                        self.slots[i] = vehicle
+                        print("Allocated slot number", i+1)
+                        break
+                else:
+                    print("Sorry! No slots avialable.")
+                    print("If need more slots:, use command:")
+                    print("'add_slot N taxi' or 'add_slot N nontaxi'")
             else:
-                print("Sorry! No slots avialable.")
-                print("If need more slots, use command: 'add_slot N'")
+                print("Please enter correct type of car.")
 
-    def addslot(self, N):
-        for i in range(N):
-            self.slots.append(0)
-        print(N, "slots added in parking lot.")
+    def addslot(self, N, taxi):
+        if taxi == "taxi":
+            for i in range(N):
+                self.slots.append(0)
+            print(N, "taxi slots added in parking lot.")
+        elif taxi == "nontaxi":
+            for i in range(N):
+                self.slots.append(-1)
+            print(N, "non taxi slots added in parking lot.")
+        else:
+            print("Please enter correct type of slot.")
 
     def leave(self, slotnumber):
         self.slots[slotnumber - 1] = 0
@@ -44,32 +72,36 @@ class Parkinglot:
     def status(self):
         print("Slot No. ", end="")
         print("Reg. No. ", end="")
-        print("Colour ")
+        print("Colour ", end="")
+        print("Taxi")
         for i in range(len(self.slots)):
-            if self.slots[i] != 0:
+            if self.slots[i] != 0 and self.slots[i] != -1:
                 print(i+1, end="        ")
                 target_car = self.slots[i]
                 print(target_car.reg_no, end="     ")
-                print(target_car.colour)
+                print(target_car.colour, end="     ")
+                print(target_car.taxi)
             else:
                 print("Slot number", i+1, "is free")
 
     def find_reg_no_by_colour(self, colour):
         car_found = []
         for i in range(len(self.slots)):
-            if self.slots[i] != 0 and self.slots[i].colour == colour:
+            c = self.slots[i]
+            if c != 0 and c != -1 and c.colour == colour:
                 target_car = self.slots[i]
                 car_found.append(target_car)
         if len(car_found) == 0:
             print("Sorry! No car of this colour is present in parking Lot.")
         else:
             for car in car_found:
-                print(car.reg_no, end=" ")
+                print(car.reg_no)
             print("")
 
     def find_car_by_reg_no(self, reg_no):
         for i in range(len(self.slots)):
-            if self.slots[i] != 0 and self.slots[i].reg_no == reg_no:
+            c = self.slots[i]
+            if c != 0 and c != -1 and c.reg_no == reg_no:
                 print(i+1)
                 break
         else:
@@ -78,7 +110,8 @@ class Parkinglot:
     def find_slots_by_colour(self, colour):
         car_found = []
         for i in range(len(self.slots)):
-            if self.slots[i] != 0 and self.slots[i].colour == colour:
+            c = self.slots[i]
+            if c != 0 and c != -1 and c.colour == colour:
                 car_found.append(i+1)
         if len(car_found) == 0:
             print("Sorry! No car of this colour is present in parking Lot.")
@@ -93,24 +126,35 @@ def main():
     print("Welcome to our Parking Lot!")
     print("These are some commands that we accept.")
     print("")
-    print("Create a parking lot of capacity N: 'create_parking_lot N'")
-    print("To park the car: 'park KA-01-HH-1234 White'")
+    print("Create a parking lot of capacity N: 'create_parking_lot N1 N2'")
+    print("N1 is the number of slots for taxi cars.")
+    print("N2 is the number of slots for non taxi cars")
+    print("")
+    print("To park the car:")
+    print("For taxi cars: 'park KA-01-HH-1234 White taxi'")
+    print("For non taxi cars: 'park KA-01-HH-1234 White nontaxi'")
+    print(" ")
     print("To take your car: 'leave 4'")
+    print("")
     print("To check status of parking Lot: 'status'")
+    print("")
     print("reg_nos of clrs:'registration_numbers_for_cars_with_colour White'")
+    print("")
     print("car by reg_no: 'slot_number_for_registration_number KA-01-HH-3141'")
+    print("")
     print("slots by colour: 'slot_numbers_for_cars_with_colour White'")
     print("")
 
     while True:
         input_cmd = input("Please enter your command:")
         if "create_parking_lot" in input_cmd:
-            cmd, cap_string = input_cmd.split()
-            capacity = int(cap_string)
-            parking_lot = Parkinglot(capacity)
+            cmd, cap_t_string, cap_nt_str = input_cmd.split()
+            capacity_taxi = int(cap_t_string)
+            cap_nt = int(cap_nt_str)
+            parking_lot = Parkinglot(capacity_taxi, cap_nt)
         elif "park" in input_cmd:
-            cmd, reg_str, colour = input_cmd.split()
-            vehicle = Car(reg_str, colour)
+            cmd, reg_str, colour, taxi = input_cmd.split()
+            vehicle = Car(reg_str, colour, taxi)
             parking_lot.park(vehicle)
         elif "status" in input_cmd:
             parking_lot.status()
@@ -118,9 +162,9 @@ def main():
             cmd, slot_no = input_cmd.split()
             parking_lot.leave(int(slot_no))
         elif "add_slot" in input_cmd:
-            cmd, noofslots_str = input_cmd.split()
+            cmd, noofslots_str, car_type = input_cmd.split()
             noofslots = int(noofslots_str)
-            parking_lot.addslot(noofslots)
+            parking_lot.addslot(noofslots, car_type)
         elif "registration_numbers_for_cars_with_colour" in input_cmd:
             cmd, colour = input_cmd.split()
             parking_lot.find_reg_no_by_colour(colour)
@@ -147,12 +191,13 @@ if __name__ == "__main__":
             for line in f:
                 input_cmd = line
                 if "create_parking_lot" in input_cmd:
-                    cmd, cap_string = input_cmd.split()
-                    capacity = int(cap_string)
-                    parking_lot = Parkinglot(capacity)
+                    cmd, cap_t_string, cap_nt_str = input_cmd.split()
+                    capacity_taxi = int(cap_t_string)
+                    cap_nt = int(cap_nt_str)
+                    parking_lot = Parkinglot(capacity_taxi, cap_nt)
                 elif "park" in input_cmd:
-                    cmd, reg_str, colour = input_cmd.split()
-                    vehicle = Car(reg_str, colour)
+                    cmd, reg_str, colour, taxi = input_cmd.split()
+                    vehicle = Car(reg_str, colour, taxi)
                     parking_lot.park(vehicle)
                 elif "status" in input_cmd:
                     parking_lot.status()
@@ -168,6 +213,10 @@ if __name__ == "__main__":
                 elif "slot_numbers_for_cars_with_colour" in input_cmd:
                     cmd, colour = input_cmd.split()
                     parking_lot.find_slots_by_colour(colour)
+                elif "add_slot" in input_cmd:
+                    cmd, noofslots_str, car_type = input_cmd.split()
+                    noofslots = int(noofslots_str)
+                    parking_lot.addslot(noofslots, car_type)
                 else:
                     print("Please enter a correct command")
     # if arguments are more than two
